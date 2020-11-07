e85d29d7c00a3f7a0026b1be1422d4b195d7a0cf
milenetoda <milene.toda@gmail.com>
2020-11-07 20:07:22 -0300

    primeiro commit

---
+++ modules/scraper.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var Xray = require("x-ray");
+var x = Xray({
+  filters: {
+    trim: function (value) {
+      return typeof value === "string" ? value.trim() : value;
+    },
+    slice: function (value, start, end) {
+      return typeof value === "string" ? value.slice(start, end) : value;
+    },
+    isShadow(value) {
+      return typeof value === "string" ? value.includes("shadow") : value;
+    },
+    getWidth: function (value) {
+      if (typeof value !== "string") {
+        return value;
+      }
+
+      const startStr = "width:";
+      const startIdx = value.indexOf(startStr);
+      if (startIdx === -1) {
+        return value;
+      }
+
+      const endStr = ";";
+      const endIdx = value.indexOf(endStr, startIdx);
+      if (endIdx === -1) {
+        return value;
+      }
+
+      return value.slice(startIdx + startStr.length, endIdx).trim();
+    },
+  },
+});
+
+module.exports = {
+  name: __filename,
+  register: async function (server, options) {
+    server.route({
+      method: "GET",
+      path: "/scrape",
+      handler: function (request, h) {
+        const url = request.query.url;
+        
+        return x(url, "#checkedIn tr[data-participant]", [
+          {
+            team: ".teamLogo@src",
+            avatar: ".avatarWrap img@src",
+            username: ".competitorUsername | trim",
+            tier: {
+              name:
+                ".player-tier div:not(.competitorUsername):not(.avatarWrap)| trim",
+              level:
+                ".player-tier div:not(.competitorUsername):not(.avatarWrap) :nth-child(2)@style | getWidth",
+            },
+            pokemon: x(".pokemonEntry", [
+              {
+                avatar: "img@src",
+                cp: "p.cp",
+                name: "p:not(.cp)",
+                shadow: "@class | isShadow",
+              },
+            ]),
+          },
+        ]);
+      },
+    });
+  },
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const Path = require("path");
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/favicon.ico",
+    handler: () => "",
+  });
+
+  await server.register(require("@hapi/inert"));
+  server.route({
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        index: ["index.html"],
+        path: Path.join(__dirname, "pages"),
+      },
+    },
+  });
+
+  await server.register(require("./modules/scraper"));
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
